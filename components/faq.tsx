6c76ee07e68a260f0a3c5b59bf03687381dbc52d
nolandex <stemapal1234@gmail.com>
2025-06-20 14:53:23 +0700

    Update faq.tsx

--- components/faq.tsx
+++ components/faq.tsx
@@ -1,62 +1,29 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-} from "@/components/ui/accordion";
+"use client";
+
+import { Accordion, AccordionContent, AccordionItem } from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { PlusIcon } from "lucide-react";
-
-const faq = [
-  {
-    question: "What is your return policy?",
-    answer:
-      "You can return unused items in their original packaging within 30 days for a refund or exchange. Contact support for assistance.",
-  },
-  {
-    question: "How do I track my order?",
-    answer:
-      "Track your order using the link provided in your confirmation email, or log into your account to view tracking details.",
-  },
-  {
-    question: "Do you ship internationally?",
-    answer:
-      "Yes, we ship worldwide. Shipping fees and delivery times vary by location, and customs duties may apply for some countries.",
-  },
-  {
-    question: "What payment methods do you accept?",
-    answer:
-      "We accept Visa, MasterCard, American Express, PayPal, Apple Pay, and Google Pay, ensuring secure payment options for all customers.",
-  },
-  {
-    question: "What if I receive a damaged item?",
-    answer:
-      "Please contact our support team within 48 hours of delivery with photos of the damaged item. We’ll arrange a replacement or refund.",
-  },
-  {
-    question: "How can I contact customer support?",
-    answer:
-      "You can reach our support team via email at support@example.com or through the live chat on our website. We're available 24/7 to assist you.",
-  },
-];
+import { config } from "@/config/data";
 
 const FAQ = () => {
+  const { faq } = config;
+
   return (
     <div id="faq" className="w-full max-w-screen-xl mx-auto py-8 xs:py-16 px-6">
       <h2 className="md:text-center text-3xl xs:text-4xl md:text-5xl !leading-[1.15] font-bold tracking-tighter">
-        Frequently Asked Questions
+        {faq.title}
       </h2>
       <p className="mt-1.5 md:text-center xs:text-lg text-muted-foreground">
-        Quick answers to common questions about our products and services.
+        {faq.description}
       </p>
-
       <div className="min-h-[550px] md:min-h-[320px] xl:min-h-[300px]">
         <Accordion
           type="single"
           collapsible
           className="mt-8 space-y-4 md:columns-2 gap-4"
         >
-          {faq.map(({ question, answer }, index) => (
+          {faq.items.map(({ question, answer }, index) => (
             <AccordionItem
               key={question}
               value={`question-${index}`}
