f46fb83a626317f41d6bffdad87ae6d2b4ba2df2
nolandex <stemapal1234@gmail.com>
2025-06-20 15:23:04 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+// Impor lainnya tetap sama
 import FAQ from "@/components/faq";
 import Features from "@/components/features";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import { Navbar } from "@/components/navbar";
 import Pricing from "@/components/pricing";
-import Testimonial from "@/components/testimonial";
+// PERBAIKI BARIS INI
+import { Testimonial } from "@/components/testimonial"; // <--- Gunakan kurung kurawal {}
 
 export default function Home() {
   return (
